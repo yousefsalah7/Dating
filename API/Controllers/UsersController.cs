6085d10828d6775429b98d76d5d51149000de83b
yousefsalah7 <yousouf.m.salah@gmail..com>
2022-11-15 17:05:11 +0200

    First Commit Controller&Database

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+   
+    public class UsersController : Controller
+    {
+        private ApplicationDbContext _db;
+        public UsersController(ApplicationDbContext applicationDbContext)
+        {
+         _db=   applicationDbContext;
+        }
+       
+        [Route("[controller]")]
+        [HttpGet]
+        public async Task< ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _db.Users.ToListAsync();
+        }
+         [Route("[controller]/{id}")]
+        [HttpGet]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _db.Users.FindAsync(id);
+        } 
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
